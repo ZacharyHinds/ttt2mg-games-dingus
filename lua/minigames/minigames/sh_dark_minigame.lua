23a730c5e14f902ef7dede0fc6c5add528443547
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-09-14 17:39:49 -0400

    Lights Out Minigame

---
+++ lua/minigames/minigames/sh_dark_minigame.lua
@@ -0,0 +1,38 @@
+if SERVER then
+  AddCSLuaFile()
+end
+
+MINIGAME.author = "Wasted"
+MINIGAME.contact = "Zzzaaaccc13 on TTT2 Discord"
+
+if CLIENT then
+  MINIGAME.lang = {
+    name = {
+      English = "Lights Out!"
+    },
+    desc = {
+      English = ""
+    }
+  }
+end
+
+if SERVER then
+  util.AddNetworkString("DarkMinigameLightmap")
+  function MINIGAME:OnActivation()
+    engine.LightStyle(0, "a")
+    net.Start("DarkMinigameLightmap")
+    net.Broadcast()
+  end
+
+  function MINIGAME:OnDeactivation()
+    engine.LightStyle(0, "m")
+    net.Start("DarkMinigameLightmap")
+    net.Broadcast()
+  end
+end
+
+if CLIENT then
+  net.Receive("DarkMinigameLightmap", function()
+    render.RedownloadAllLightmaps()
+  end)
+end
