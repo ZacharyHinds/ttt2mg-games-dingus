f9f4a95dd5c59f9f8b7e0614295b072504fd48b1
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-09-15 11:14:12 -0400

    Medieval Minigame improvements

---
+++ lua/minigames/minigames/sh_medieval_minigame.lua
@@ -0,0 +1,51 @@
+if SERVER then
+  AddCSLuaFile()
+end
+
+MINIGAME.author = "Wasted"
+MINIGAME.contact = "Zzzaaaccc13 on TTT2 Discord"
+
+if CLIENT then
+  MINIGAME.lang = {
+    name = {
+      English = "Trouble in Ye Olde Terrorist Towne"
+    },
+    desc = {
+      English = ""
+    }
+  }
+end
+
+if SERVER then
+  function MINIGAME:OnActivation()
+    local ents = ents.GetAll()
+
+    for i = 1, #ents do
+      local ent = ents[i]
+      if ent.Base == "weapon_tttbase" and ent.AutoSpawnable then
+        ent:Remove()
+      end
+    end
+    local plys = util.GetAlivePlayers()
+    local wep
+    for i = 1, #plys do
+      local ply = plys[i]
+      local weps = ply:GetWeapons()
+      for j = 1, #weps do
+        wep = weps[j]
+        if wep.Kind == WEAPON_HEAVY or wep.Kind == WEAPON_PISTOL then
+          ply:StripWeapon(wep:GetClass())
+        end
+      end
+      ply:GiveEquipmentWeapon("weapon_ttt_mg_bow")
+      ply:GiveEquipmentWeapon("weapon_ttt_shovel")
+    end
+  end
+
+  function MINIGAME:OnDeactivation()
+    local plys = util.GetAlivePlayers()
+    for i = 1, #plys do
+      plys[i]:StripWeapon("weapon_ttt_mg_bow")
+    end
+  end
+end
