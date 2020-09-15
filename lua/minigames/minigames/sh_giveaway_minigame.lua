0ac5ffd25bbcee1fe74d45d0bd4864af2c919fd2
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-09-15 13:42:31 -0400

    Giveaway Minigame

---
+++ lua/minigames/minigames/sh_giveaway_minigame.lua
@@ -0,0 +1,78 @@
+if SERVER then
+  AddCSLuaFile()
+end
+
+MINIGAME.author = "Wasted"
+MINIGAME.contact = "Zzzaaaccc13 on TTT2 Discord"
+
+if CLIENT then
+  MINIGAME.lang = {
+    name = {
+      English = "Sweepstakes Giveaway"
+    },
+    desc = {
+      English = "You've all won a prize!"
+    }
+  }
+end
+
+if SERVER then
+  util.AddNetworkString("ttt2mg_giveaway")
+
+  function MINIGAME:OnActivation()
+    local weps = weapons.GetList()
+    local give_weps = {}
+    for i = 1, #weps do
+      local wep = weps[i]
+      if wep and wep.CanBuy then
+        give_weps[#give_weps + 1] = wep
+      end
+    end
+    local item
+    local is_item
+    local swep_table
+    repeat
+      if #weps <= 0 then return end
+      local rnd = math.random(#give_weps)
+      item = give_weps[rnd]
+      is_item = tonumber(item.id)
+      swep_table = (not is_item) and weapons.GetStored(item.ClassName) or nil
+      table.remove(give_weps, rnd)
+    until IsValid(item) and (is_item or swep_table)
+    local print_name = item
+    if item.PrintName then
+      print_name = item.PrintName
+    end
+
+    local plys = util.GetAlivePlayers()
+    for i = 1, #plys do
+      local ply = plys[i]
+      if not IsValid(ply) then continue end
+      if is_item then
+        ply:GiveEquipmentItem(is_item)
+      elseif swep_table then
+        ply:GiveEquipmentWeapon(swep_table)
+      end
+    end
+    net.Start("ttt2mg_giveaway")
+    net.WriteString(print_name)
+    net.Broadcast()
+  end
+
+  function MINIGAME:OnDeactivation()
+
+  end
+end
+if CLIENT then
+  net.Receive("ttt2mg_giveaway", function()
+    local wep_name = net.ReadString()
+    EPOP:AddMessage({
+      text = "Today's Giveaway is..."},
+      "",
+    3)
+    EPOP:AddMessage({
+      text = wep_name},
+      "",
+    2)
+  end)
+end
