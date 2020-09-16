10508823219307818e43b68d16542bc741796203
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-09-16 14:10:58 -0400

    Better cleanup, fixed missing not statement

--- lua/minigames/minigames/sh_haunting_minigame.lua
+++ lua/minigames/minigames/sh_haunting_minigame.lua
@@ -36,8 +36,10 @@ if SERVER then
     end)
     local tick_delay = CurTime() + 3
     hook.Add("Think", "ghostMinigameUpdate", function()
-      for _, ply in ipairs(player.GetAll()) do
-        if ply:Alive() or ply:IsSpec() then continue end
+      local plys = player.GetAll()
+      for i = 1, #plys do
+        local ply = plys[i]
+        if ply:Alive() or not ply:IsSpec() then continue end
         if not ply:GetNWBool("IsSpooky") then continue end
         local hasUpdated = false
         if CurTime() >= tick_delay and not hasUpdated then
@@ -53,5 +55,12 @@ if SERVER then
   function MINIGAME:OnDeactivation()
     hook.Remove("Think", "ghostMinigameUpdate")
     hook.Remove("TTT2PostPlayerDeath", "HauntingMinigame")
+    local plys = player.GetAll()
+    for i = 1, #plys do
+      plys[i]:SetNWBool("IsSpooky", false)
+    end
+    for i = 1, #ghostents do
+      ghostents[i]:Remove()
+    end
   end
 end
